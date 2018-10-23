a0292a43efeab3c86c8ed0b9170023db2d53bbd1
NilssonKristian <nilsson_kristian@outlook.com>
2018-10-23 14:18:15 +0200

    jag la till en ny kommentar från koranen

--- podcastRSS/podcastRSS/Form1.cs
+++ podcastRSS/podcastRSS/Form1.cs
@@ -24,7 +24,7 @@ namespace podcastRSS
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            //det här en annan kommentar... Kristian har skrivit detta. jag lovar och svär på koranen
         }
 
         private void button1_Click(object sender, EventArgs e)
